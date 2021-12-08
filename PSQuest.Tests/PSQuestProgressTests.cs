577bc107a9d42fb59e5588eed20bc757aff30742
tusharvasani <77061918+tusharvasani@users.noreply.github.com>
2021-12-09 00:39:03 +0800

    Added xUnit project for Unit Tests

---
+++ PSQuest.Tests/PSQuestProgressTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PSQuest.Tests
+{
+    public class PSQuestProgressTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
